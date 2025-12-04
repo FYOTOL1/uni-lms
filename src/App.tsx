de250a4e580dc3e6fec177fa09c01fec27366747
Ahmed Akl <fyotol@example.com>
2025-12-04 14:10:18 +0200

    First Deployment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full bg-gray-900">
+      <button className="bg-blue-400 shadow px-4 py-2 text-xl rounded-md text-white hover:bg-blue-500 cursor-pointer transition-all">
+        <a href="https://whatsapp.com/channel/0029VadqQxz1Hsq1c0YFmT0G">
+          Whatsapp Group
+        </a>
+      </button>
+    </div>
+  );
+};
+
+export default App;

b1099156730d80eb43625aa5d16f77b5443b341b
Ahmed Akl <fyotol@example.com>
2025-12-04 14:16:01 +0200

    First Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full bg-gray-900">
       <button className="bg-blue-400 shadow px-4 py-2 text-xl rounded-md text-white hover:bg-blue-500 cursor-pointer transition-all">
-        <a href="https://whatsapp.com/channel/0029VadqQxz1Hsq1c0YFmT0G">
+        <a href="https://chat.whatsapp.com/EC6esBs7PfXHmQ7rfTGz9C">
           Whatsapp Group
         </a>
       </button>

a9cebaba05e4c1b7fc23124dc8b610ad03e93287
Ahmed Akl <fyotol@gmail.com>
2026-01-02 13:55:51 +0200

    Published

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SignupForm from "./forms/signup/SignupForm";
 
 const App = () => {
   return (
-    <div className="relative flex justify-center items-center min-h-screen w-full bg-[#5611c4]">
+    <div className="relative flex justify-center items-center min-h-screen w-full bg-[#eee]">
       <SignupForm />
     </div>
   );

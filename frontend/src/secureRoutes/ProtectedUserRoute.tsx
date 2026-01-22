db505afb894e081f6f0fae6a561d3b5d8cb78552
Ahmed Akl <fyotol@gmail.com>
2026-01-22 03:53:40 +0200

    last

--- frontend/src/secureRoutes/ProtectedUserRoute.tsx
+++ frontend/src/secureRoutes/ProtectedUserRoute.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useNavigate } from "react-router";
+import { Navigate } from "react-router";
 import useAuth from "../hooks/useAuth";
 import type { TMeRequest } from "../types/auth/meTypes";
 
@@ -8,24 +8,20 @@ const ProtectedUserRoute = ({
 }: {
   children: (user: TMeRequest) => any | void;
 }) => {
-  const navigate = useNavigate();
   const { user, isLoading, isLogged } = useAuth();
 
-  if (isLoading && user)
+  if (isLoading)
     return (
       <div className="relative h-screen w-full">
         <div className="absolute h-full w-full bg-black opacity-50 z-20"></div>
         <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-30 text-2xl">
           Loading...
         </div>
-        <div className="z-10">{children(user)}</div>
+        <div className="z-10">{children(user!)}</div>
       </div>
     );
 
-  if (!isLogged) {
-    navigate("/auth/login", { replace: true });
-    return <></>;
-  }
+  if (!isLogged) return <Navigate to={"/auth/login"} replace />;
 
   return children(user);
 };

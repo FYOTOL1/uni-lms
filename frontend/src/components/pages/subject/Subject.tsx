833008e641cb4c7f1cf573ba6a26c54f3e31b4da
Ahmed Akl <fyotol@gmail.com>
2026-01-27 07:21:54 +0200

    a

--- frontend/src/components/pages/subject/Subject.tsx
+++ frontend/src/components/pages/subject/Subject.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router";
 import type { TSubjectSchemaType } from "../../../types/schema/SubjectSchemaType";
 
 const subject = ({ subject }: { subject: TSubjectSchemaType }) => {
-  return (
+  
+  
+  return subject && (
     <>
       <div className="flex flex-col gap-4 h-full w-full">
         <div>

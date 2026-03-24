a3b1e30332d9d019d968fb2742edc7c118616808
Ahmed Akl <fyotol@gmail.com>
2026-03-24 06:31:43 +0200

    tets

--- frontend/src/components/pages/user/subject/AssignmentCard.tsx
+++ frontend/src/components/pages/user/subject/AssignmentCard.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { TAssignmentSchemaType } from "../../../../types/schema/AssignmentSchemaType";
 
 const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
-  const [now, setNow] = useState(() => Date.now());
+  const [now, setNow] = useState(() => Date?.now());
 
   const getLeftTime = (createdAt: Date | string, deadline: Date | string) => {
     const createdAtTime = new Date(createdAt).getTime();
@@ -29,7 +29,7 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const now = Date.now();
+      const now = Date?.now();
       setNow(now);
     }, 1000);
 
@@ -38,7 +38,7 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
 
   return (
     <div
-      key={e._id}
+      key={e?._id}
       className="flex flex-col gap-3 p-4 rounded-md bg-white outline outline-gray-100 shadow-lg"
     >
       {/* Header */}
@@ -49,23 +49,23 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
         <div className="flex flex-col gap-2">
           <div className="flex items-center gap-1">
             <p className="px-3 py-1 rounded-full bg-gray-200 text-xs text-gary-700 font-semibold shadow-sm">
-              {e.subject.subjectCode}
+              {e?.subject.subjectCode}
             </p>
             <p className="px-3 py-1 rounded-full bg-purple-200 text-purple-600 text-xs  font-semibold uppercase shadow-sm">
-              {e.group}
+              {e?.group}
             </p>
             <p className="px-3 py-1 rounded-full bg-blue-200 text-blue-600 text-xs font-semibold uppercase shadow-sm">
-              {e.group}-{e.sectionNumber}
+              {e?.group}-{e?.sectionNumber}
             </p>
           </div>
           <h1 className="text-[16px] text-gray-800 font-semibold capitalize text-wrap">
-            {e.assignmentTitle}
+            {e?.assignmentTitle}
           </h1>
         </div>
       </div>
 
       {/* Description */}
-      <p className="text-sm text-gray-800 text-wrap my-2">{e.assignmentDesc}</p>
+      <p className="text-sm text-gray-800 text-wrap my-2">{e?.assignmentDesc}</p>
 
       {/* Dates */}
       <div className="flex items-center gap-2">
@@ -73,7 +73,7 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
           <i className="fa-regular fa-calendar-plus text-gray-400 text-sm" />
           <p>
             Added:{" "}
-            {`${new Date(e.createdAt).getDate()} ${new Date(e.createdAt).toLocaleString("en-US", { month: "short" })}`}
+            {`${new Date(e?.createdAt).getDate()} ${new Date(e?.createdAt).toLocaleString("en-US", { month: "short" })}`}
           </p>
         </div>
 
@@ -81,7 +81,7 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
           <i className="fa-regular fa-calendar text-gray-400 text-sm" />
           <p>
             Deadline:{" "}
-            {`${new Date(e.deadline).getDate()} ${new Date(e.deadline).toLocaleString("en-US", { month: "short" })}`}
+            {`${new Date(e?.deadline).getDate()} ${new Date(e?.deadline).toLocaleString("en-US", { month: "short" })}`}
           </p>
         </div>
       </div>
@@ -90,7 +90,7 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
       <div className="relative w-full h-2 rounded-full bg-gray-100 outline outline-gray-200 overflow-hidden">
         <div
           style={{
-            width: `${getLeftTime(e.createdAt, e.deadline)}%`,
+            width: `${getLeftTime(e?.createdAt, e?.deadline)}%`,
           }}
           className={`absolute left-0 h-full bg-linear-90 from-pink-400 to-purple-600`}
         ></div>
@@ -101,19 +101,19 @@ const AssignmentCard = ({ e }: { e: TAssignmentSchemaType }) => {
           <i className="fa-regular fa-calendar text-gray-400" />
           <p className="text-gray-600">
             Deadline:{" "}
-            {getDaysLeft(e.deadline) === 1
-              ? getDaysLeft(e.deadline) + " Day"
-              : getDaysLeft(e.deadline) > 1
-                ? getDaysLeft(e.deadline) + " Days"
+            {getDaysLeft(e?.deadline) === 1
+              ? getDaysLeft(e?.deadline) + " Day"
+              : getDaysLeft(e?.deadline) > 1
+                ? getDaysLeft(e?.deadline) + " Days"
                 : "Time"}{" "}
             Left
           </p>
         </div>
 
         <p
-          className={`flex items-center justify-center px-2 py-1 rounded-full ${getDaysLeft(e.deadline) === 0 ? "bg-gray-200 text-gray-600" : "bg-purple-500 text-white"} font-semibold`}
+          className={`flex items-center justify-center px-2 py-1 rounded-full ${getDaysLeft(e?.deadline) === 0 ? "bg-gray-200 text-gray-600" : "bg-purple-500 text-white"} font-semibold`}
         >
-          {getDaysLeft(e.deadline) === 0 ? "Not Active" : "Active"}
+          {getDaysLeft(e?.deadline) === 0 ? "Not Active" : "Active"}
         </p>
       </div>
     </div>
